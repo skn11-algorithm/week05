6051d20de5854774d6fbaed4ec650e272a82b252
Ohjunghh <cmt5ttt@gmail.com>
2025-03-23 01:33:22 +0900

    [ojh] boj: complete 18870.py - reduced code length

--- ojh/18870.py
+++ ojh/18870.py
@@ -2,15 +2,5 @@ import sys
 input=sys.stdin.readline
 n=int(input())
 nums=list(map(int,input().rstrip().split()))
-
-# 중복 처리 # 10 9 10 9
-index_map={}
-for i,num in enumerate(sorted(set(nums))): # 중복 없애고 정렬 # 9 10
-    index_map[num]=i # 9 : 0, 10 : 1
-
-for i in range(n):
-    nums[i]=index_map[nums[i]]
-print(*nums)
-
-
-
+result={num:i for i,num in enumerate(sorted(set(nums)))}
+print(' '.join(map(str,[result[num] for num in nums])))
